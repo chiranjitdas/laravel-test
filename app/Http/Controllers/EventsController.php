46b03d4181f99651e8a6b2e1f3dfb977fa5e69dc
Chiranjit Das <chiranjit1808@gmail.com>
2021-04-28 21:54:49 +0530

    Task 1 done

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -8,11 +8,21 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+
 
 class EventsController extends BaseController
 {
     public function getEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 1');
+        // throw new \Exception('implement in coding task 1');
+        $workshops = DB::table('workshops')
+                   ->select('*')
+                   ->groupBy('event_id');
+        $users = DB::table('events')
+        ->joinSub($workshops, 'workshops', function ($join) {
+            $join->on('events.id', '=', 'workshops.event_id');
+        })->get();
+        return response()->json(json_decode($users));
     }
 
     public function getWarmUpEvents() {
